172328d5425e888713997197f842e8ed600a2cc3
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-15 14:25:02 -0300

    Busca por identificador

--- src/test/java/com/especialista/junit/_3_inciandoComJpa/_1_ConsultandoRegistroTest.java
+++ src/test/java/com/especialista/junit/_3_inciandoComJpa/_1_ConsultandoRegistroTest.java
@@ -1,8 +1,7 @@
 package com.especialista.junit._3_inciandoComJpa;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.especialista.jpa._2_iniciandoComJPA.modelo.Produto;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,7 +14,7 @@ public class _1_ConsultandoRegistroTest {
 
     @BeforeClass
     public static void setUpBeforeClass(){ // executa primeiro
-        System.out.println(">>> Inciando o EntityManagerFactory...");
+        System.out.println(">>> Iniciando o EntityManagerFactory...");
         entityManagerFactory = Persistence.createEntityManagerFactory("EspecialistaJPADB-PU");
 
     }
@@ -37,5 +36,42 @@ public class _1_ConsultandoRegistroTest {
     public void tearDown(){ // executa depois de cada teste
         System.out.println(">>> Finalizando o EntityManager...");
         entityManager.close();
+    }
+
+
+    @Test
+    public void buscarPorId(){
+        System.out.println("1. Buscando produto pelo id...");
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(">>> Já fez a consulta!!!");
+
+        Assert.assertNotNull("Produto não pode ser nulo", produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void buscarPorReferencia(){
+        System.out.println("2. Buscando produto pelo id usando referência...");
+
+        Produto produto = entityManager.getReference(Produto.class, 1);
+        System.out.println(">>> Ainda não fez na consulta!!!");
+
+        Assert.assertNotNull("Produto não pode ser nulo", produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+
+    @Test
+    public void atualizarAReferencia(){
+        System.out.println("3. Atualizando a referência...");
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson"); // dessa salva não será salvo no banco
+
+        System.out.println(">>> Atualizando o estado da instância do banco de dados, sobrescrevendo as alterações feitas na entidade, se houver");
+        entityManager.refresh(produto);
+
+        Assert.assertEquals("Kindle", produto.getNome());
     }
 }
