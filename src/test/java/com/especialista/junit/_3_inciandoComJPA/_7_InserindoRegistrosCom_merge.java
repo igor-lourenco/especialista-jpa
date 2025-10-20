9b5a743b22d824fce03d002cb389e29784455e1c
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-20 10:41:16 -0300

    Entendendo a diferença entre os métodos persist e merge

--- src/test/java/com/especialista/junit/_3_inciandoComJPA/_7_InserindoRegistrosCom_merge.java
+++ src/test/java/com/especialista/junit/_3_inciandoComJPA/_7_InserindoRegistrosCom_merge.java
@@ -12,29 +12,74 @@ public class _7_InserindoRegistrosCom_merge extends EntityManagerTest {
 
 
     @Test
-    public void inserindoObjetoComMerge() {
+    public void mostrandoDiferencaEntrePersistEMerge() {
+        System.out.println("=========================================================");
+        System.out.println("======================== persist ========================");
+        System.out.println("=========================================================");
+
         System.out.println(">>> 1. Instanciando o produto...");
 
-        Produto produto = new Produto(); // os atributos que não estiver preenchido o JPA irá salvar como null
-        produto.setId(4);
-        produto.setNome("Microfone Rode Videmic");
-        produto.setDescricao("Melhor qualidade de som");
-        produto.setPreco(new BigDecimal("1000"));
+        Produto produtoPersist = new Produto(); // os atributos que não estiver preenchido o JPA irá salvar como null
+        produtoPersist.setId(5);
+        produtoPersist.setNome("Smartphone One Plus");
+        produtoPersist.setDescricao("O processador mais rápido");
+        produtoPersist.setPreco(new BigDecimal("2000"));
 
         entityManager.getTransaction().begin(); // Início da transação
 
-//      O método merge permite atualizar e também inserir
-        entityManager.merge(produto);
 
-        System.out.println(">>> 2. Fazendo a atualização do produto no banco de dados...");
+        System.out.println(">>> 2. Fazendo a inserção do produto no banco de dados e no retorno coloca o objeto na " +
+            "memória para ser gerenciada pelo EntityManager...");
+        entityManager.persist(produtoPersist); // Cria uma instância do objeto e coloca na memória para ser gerenciada e persistente
+
+        System.out.println(">>> 3. Fazendo a atualização do produto no banco de dados na mesma transação...");
+        produtoPersist.setNome("Smartphone Two Plus");
+
+
         entityManager.getTransaction().commit(); // Fim da transação (confirma a transação)
 
 //      Limpa o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas.
         entityManager.clear();
 
-        System.out.println(">>> 3. Fazendo a consulta no banco de dados para verificar se o produto foi criado...");
-        Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
-        Assert.assertNotNull(produtoVerificado);
-        Assert.assertEquals("Microfone Rode Videmic", produtoVerificado.getNome());
+        System.out.println(">>> 4. Fazendo a consulta no banco de dados para verificar se o produto foi atualizado...");
+        Produto produtoVerificadoPersist = entityManager.find(Produto.class, produtoPersist.getId());
+        Assert.assertNotNull(produtoVerificadoPersist);
+        Assert.assertEquals("Smartphone Two Plus", produtoVerificadoPersist.getNome());
+
+
+        System.out.println("=========================================================");
+        System.out.println("======================== merge ========================");
+        System.out.println("=========================================================");
+
+
+        System.out.println(">>> 5. Instanciando o produto...");
+
+        Produto produtoMerge = new Produto(); // os atributos que não estiver preenchido o JPA irá salvar como null
+        produtoMerge.setId(6);
+        produtoMerge.setNome("Notebook Dell");
+        produtoMerge.setDescricao("O melhor da categoria");
+        produtoMerge.setPreco(new BigDecimal("2000"));
+
+        entityManager.getTransaction().begin(); // Início da transação
+
+
+        System.out.println(">>> 6. Fazendo a inserção do produto no banco de dados e no retorno cria uma cópia do objeto" +
+            " e coloca na memória para ser gerenciada pelo EntityManager...");
+        produtoMerge = entityManager.merge(produtoMerge);
+
+        System.out.println(">>> 7. Fazendo a atualização da cópia do objeto produto que foi retornado pelo merge...");
+//      Não vai atualizar porque o EntityManager está gerenciando a cópia desse objeto que foi retornado pelo merge
+        produtoMerge.setNome("Smartphone Two Plus");
+
+
+        entityManager.getTransaction().commit(); // Fim da transação (confirma a transação)
+
+//      Limpa o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas.
+        entityManager.clear();
+
+        System.out.println(">>> 8. Fazendo a consulta no banco de dados para verificar se o produto foi atualizado...");
+        Produto produtoVerificadoMerge = entityManager.find(Produto.class, produtoMerge.getId());
+        Assert.assertNotNull(produtoVerificadoMerge);
+        Assert.assertEquals("Smartphone Two Plus", produtoVerificadoMerge.getNome());
     }
 }
