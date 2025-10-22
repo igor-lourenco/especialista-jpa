ecbcfc84aacc5d181301fa638021d84195475718
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-22 11:08:35 -0300

    Estratégias para chave primária com @GeneratedValue

--- src/main/java/com/especialista/jpa/_3_mapeamentoBasico/modelos/Categoria.java
+++ src/main/java/com/especialista/jpa/_3_mapeamentoBasico/modelos/Categoria.java
@@ -4,10 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -18,6 +15,10 @@ public class Categoria {
 
     @Id
     @EqualsAndHashCode.Include
+
+//  Especifica que a implementação do JPA(Hibernate) vai escolher a forma(estratégia) mais apropriada que o id vai ser atribuído,
+//  pode usar: IDENTITY, SEQUENCE ou TABLE
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String nome;
---
+++ src/test/java/com/especialista/junit/_4_mapeamentoBasico/_3_EstrategiasParaChavePrimariaCom_generatedValue.java
@@ -0,0 +1,37 @@
+package com.especialista.junit._4_mapeamentoBasico;
+
+import com.especialista.jpa._3_mapeamentoBasico.modelos.Categoria;
+import com.especialista.jpa._3_mapeamentoBasico.modelos.Endereco;
+import com.especialista.jpa._3_mapeamentoBasico.modelos.Pedido;
+import com.especialista.jpa._3_mapeamentoBasico.modelos.StatusPedido;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class _3_EstrategiasParaChavePrimariaCom_generatedValue extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategia_GenerationType_AUTO(){
+        System.out.println("\n>>> 1. Instanciando a categoria...");
+
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        System.out.println("\n>>> 2. Buscando na tabela 'hibernate_sequence' o valor do id ...");
+        entityManager.getTransaction().begin(); // Início da transação
+
+        entityManager.persist(categoria);
+
+        System.out.println("\n>>> 3. Fazendo a inserção da nova categoria no banco de dados...");
+        entityManager.getTransaction().commit(); // Fim da transação (confirma a transação)
+
+        entityManager.clear(); //Limpa o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas.
+
+        System.out.println("\n>>> 4. Fazendo a consulta da categoria no banco de dados...");
+        Categoria categoriaVerificado = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificado);
+    }
+}
