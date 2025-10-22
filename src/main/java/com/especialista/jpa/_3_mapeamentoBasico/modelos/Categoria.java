93adc589888007c5265d6e07c1a0cdd426a0ffe8
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-22 14:44:31 -0300

    Configurando a geração de identificador com @TableGenerator

--- src/main/java/com/especialista/jpa/_3_mapeamentoBasico/modelos/Categoria.java
+++ src/main/java/com/especialista/jpa/_3_mapeamentoBasico/modelos/Categoria.java
@@ -16,10 +16,13 @@ public class Categoria {
     @Id
     @EqualsAndHashCode.Include
 
-//  Usa uma sequência nativa do banco de dados para gerar os valores da chave primária.
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq",
-        sequenceName = "tb_categoria_sequencias_chave_primaria", // nome da sequência no banco
+//  Usa uma tabela auxiliar para gerar IDs
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela")
+    @TableGenerator(name = "tabela",
+        table = "hibernate_sequences", // nome da tabela que armazenará os valores dos IDs
+        pkColumnName = "sequence_name", // nome da coluna que identifica o tipo de entidade
+        pkColumnValue = "tb_categoria", // valor que será usado na coluna pkColumnName para identificar a sequência específica dessa entidade.
+        valueColumnName = "next_value", // nome da coluna que armazena o valor atual do ID.
         allocationSize = 100, // define quantos valores o JPA reserva por vez em cache. (1 = sem cache).
         initialValue = 0 // valor inicial
     )
