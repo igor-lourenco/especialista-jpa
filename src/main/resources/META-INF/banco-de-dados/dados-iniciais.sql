24f4f53d76a1fad9f16791220188e519c918c8a6
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-24 11:23:52 -0300

    Removendo objetos referenciados

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -11,3 +11,7 @@ insert into tb_cliente(id, nome) values (2, 'Marcos Mariano');
 
 insert into tb_produto(id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 insert into tb_produto(id, nome, preco, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
+
+insert into tb_pedido (id, cliente_id, data_pedido, total, status) values (1, 1, sysdate(), 100.0, 'AGUARDANDO');
+
+insert into tb_item_pedido (id, pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 1, 5.0, 2);
---
+++ src/test/java/com/especialista/junit/_5_mapeamentoDeRelacionamentos/_6_RemovendoObjetosReferenciados.java
@@ -0,0 +1,38 @@
+package com.especialista.junit._5_mapeamentoDeRelacionamentos;
+
+import com.especialista.jpa._4_mapeamentoDeRelacionamento.modelos.Categoria;
+import com.especialista.jpa._4_mapeamentoDeRelacionamento.modelos.Pedido;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _6_RemovendoObjetosReferenciados extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento_ManyToOne(){
+        System.out.println("\n>>> 1. Buscando pedido na base de dados...");
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItensPedido().isEmpty());
+
+        entityManager.getTransaction().begin(); // Início da transação
+
+        System.out.println("\n>>> 4. Fazendo a remoção dos itemPedido no banco de dados...");
+        pedido.getItensPedido().forEach(entityManager::remove);
+
+        System.out.println("\n>>> 5. Fazendo a remoção do pedido no banco de dados...");
+        entityManager.remove(pedido);
+
+
+        entityManager.getTransaction().commit(); // Fim da transação (confirma a transação)
+
+//      Limpa o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas.
+//      entityManager.clear(); // Não é necessário para operação de remoção
+
+        System.out.println("\n>>> 6. Fazendo a consulta do pedido no banco de dados...");
+        Pedido pedidoVerificado = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNull(pedidoVerificado);
+    }
+
+
+}
