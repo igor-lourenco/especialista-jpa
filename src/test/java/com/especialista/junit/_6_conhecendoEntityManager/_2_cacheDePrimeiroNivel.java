1f0fb71b0a6f5881add2a909ec39346f6b8f656f
igor-lourenco <igor.lourencosantos@gmail.com>
2025-10-28 11:00:18 -0300

    Cache de primeiro nível

---
+++ src/test/java/com/especialista/junit/_6_conhecendoEntityManager/_2_cacheDePrimeiroNivel.java
@@ -0,0 +1,24 @@
+package com.especialista.junit._6_conhecendoEntityManager;
+
+import com.especialista.jpa._5_conhecendoEntityManager.modelos.Produto;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Test;
+
+public class _2_cacheDePrimeiroNivel extends EntityManagerTest {
+
+
+    @Test
+    public void verificarCache() {
+        System.out.println("\n>>> 1. Buscando o produto no banco de dados...");
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println("\nNome produto: " + produto.getNome());
+
+        System.out.println("------------------------------------------------------");
+
+        System.out.println(">>> 2. Buscando o mesmo produto do cache de primeiro nível...");
+        Produto produtoResgatado = entityManager.find(Produto.class, produto.getId());
+        System.out.println("Nome produto resgatado: " + produtoResgatado.getNome());
+
+    }
+}
