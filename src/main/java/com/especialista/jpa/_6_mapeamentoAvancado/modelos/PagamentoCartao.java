6c23c82660d9fe9f8cc0ae63f76f0bb6a2002896
igor-lourenco <igor.lourencosantos@gmail.com>
2025-11-03 11:13:44 -0300

    Exercicio usando o @MapsId

--- src/main/java/com/especialista/jpa/_6_mapeamentoAvancado/modelos/PagamentoCartao.java
+++ src/main/java/com/especialista/jpa/_6_mapeamentoAvancado/modelos/PagamentoCartao.java
@@ -15,7 +15,8 @@ public class PagamentoCartao {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //  Usa auto-incremento do banco
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) //  Usa auto-incremento do banco
+    @Column(name = "pedido_id") // coluna deve ser o mesmo do atributo mapeado com @MapsId
     private Integer id;
 
     @Enumerated(EnumType.STRING) // Salva a String do enum no banco de dados
@@ -23,6 +24,8 @@ public class PagamentoCartao {
 
     private String numero;
 
+    //  O nome do atributo dentro da chave composta ao qual o atributo de relacionamento corresponde. Se não for fornecido, o relacionamento mapeia a chave primária da entidade.
+    @MapsId // Especifica ao JPA que um ou mais campos da PK vêm do identificador de uma associação (@ManyToOne ou @OneToOne), evitando duplicidade de colunas e mantendo tudo sincronizado.
     @OneToOne(fetch = FetchType.EAGER, optional = false) // um pagamentoCartao tem um pedido, por padrão usa o Fetch.EAGER
     @JoinColumn(name = "pedido_id") // especifica uma coluna para unir as associações. (owner)
     private Pedido pedido;
---
+++ src/test/java/com/especialista/junit/_7_mapeamentoAvancado/_6_exercicioMapeandoChavePrimariaEChaveEstrangeiraNaMesmaColunaCom_MapsId.java
@@ -0,0 +1,110 @@
+package com.especialista.junit._7_mapeamentoAvancado;
+
+import com.especialista.jpa._6_mapeamentoAvancado.modelos.*;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+public class _6_exercicioMapeandoChavePrimariaEChaveEstrangeiraNaMesmaColunaCom_MapsId extends EntityManagerTest {
+
+
+    @Test
+    public void inserirPagamento(){
+        System.out.println("\n>>> 1. Buscando Pedido no banco de dados...");
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        System.out.println("\n>>> 2. Instânciando novo PagamentoCartao...");
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setNumero("1234");
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setPedido(pedido);
+
+        System.out.println("\n>>> 3. Iniciando uma transação...");
+        entityManager.getTransaction().begin();// Início da transação
+
+        System.out.println("\n>>> 4. Colocando uma nova NotaFiscal no contexto de persistência usando o persist()...");
+        entityManager.persist(pagamentoCartao);
+
+        System.out.println("\n>>> 5. JPA confirmando a transação, salvando as alterações no banco de dados...");
+        entityManager.getTransaction().commit();// Fim da transação (confirma a transação)
+
+        System.out.println("\n>>> 6. Limpando o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas...");
+        entityManager.clear();
+
+        System.out.println("\n>>> 7. Buscando PagamentoCartao criado no banco de dados...");
+        PagamentoCartao pagamentoCartaoVerificado = entityManager.find(PagamentoCartao.class, pedido.getId());
+
+        Assert.assertNotNull(pagamentoCartaoVerificado);
+        Assert.assertEquals(pagamentoCartaoVerificado.getId(), pedido.getId());
+    }
+
+
+    @Test
+    public void salvarItemPedido() {
+        System.out.println("\n>>> 1. Buscando Cliente no banco de dados...");
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        System.out.println("\n>>> 2. Buscando Produto no banco de dados...");
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        System.out.println("\n>>> 3. Instânciando novo Pedido...");
+        Pedido pedido = new Pedido();
+        pedido.setCliente(cliente);
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+
+        System.out.println("\n>>> 4. Iniciando uma transação...");
+        entityManager.getTransaction().begin();// Início da transação
+
+        System.out.println("\n>>> 5. Colocando uma novo Pedido no contexto de persistência usando o persist()...");
+        entityManager.persist(pedido);
+
+        System.out.println("\n>>> 6. Sincronizando as alterações feitas na entidade com o banco de dados...");
+        entityManager.flush();
+
+        System.out.println("\n>>> 7. Instânciando novo PagamentoCartao...");
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setNumero("1234");
+        pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
+        pagamentoCartao.setPedido(pedido);
+
+        System.out.println("\n>>> 8. Colocando uma novo Pedido no contexto de persistência usando o persist()...");
+        entityManager.persist(pagamentoCartao);
+
+        System.out.println("\n>>> 9. Sincronizando as alterações feitas na entidade com o banco de dados...");
+        entityManager.flush();
+
+        System.out.println("\n>>> 10. Instânciando novo ItemPedido...");
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setId(new ItemPedidoId());
+
+
+        System.out.println("\n>>> 11. Colocando um novo ItemPedido no contexto de persistência usando o persist()...");
+        entityManager.persist(itemPedido);
+
+        System.out.println("\n>>> 12. JPA confirmando a transação, salvando as alterações no banco de dados...");
+        entityManager.getTransaction().commit();// Fim da transação (confirma a transação)
+
+        System.out.println("\n>>> 13. Limpando o contexto de persistência, fazendo com que todas as entidades gerenciadas sejam desanexadas...");
+        entityManager.clear();
+
+        System.out.println("\n>>> 14. Buscando Pedido criado no banco de dados...");
+        Pedido pedidoVerificado = entityManager.find(Pedido.class, pedido.getId());
+
+        Assert.assertNotNull(pedidoVerificado);
+        Assert.assertFalse(pedidoVerificado.getItensPedido().isEmpty());
+
+        System.out.println("\n>>> 7. Buscando PagamentoCartao criado no banco de dados...");
+        PagamentoCartao pagamentoCartaoVerificado = entityManager.find(PagamentoCartao.class, pedido.getId());
+
+        Assert.assertNotNull(pagamentoCartaoVerificado);
+        Assert.assertEquals(pagamentoCartaoVerificado.getId(), pedido.getId());
+
+    }
+}
