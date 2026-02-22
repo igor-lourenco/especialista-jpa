13fd3d880455406a60f366d72d88d0255664434a
igor-lourenco <igor.lourencosantos@gmail.com>
2026-02-22 14:11:31 -0300

    Limitando a quantidade de registros retornandos

---
+++ src/test/java/com/especialista/junit/_10_JPQL/_21_Limitando_quantidade_de_registros_retornados.java
@@ -0,0 +1,31 @@
+package com.especialista.junit._10_JPQL;
+
+import com.especialista.jpa._7_mapeandoEntidadesParaGeracaoDeDDL.modelos.Pedido;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class _21_Limitando_quantidade_de_registros_retornados extends EntityManagerTest {
+
+    @Test
+    public void paginarResultados() {
+        String jpql = "SELECT p FROM Pedido p "
+            + " ORDER BY p.id ASC";
+
+
+        logger.info("Buscando Pedidos ...");
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class)
+            .setMaxResults(10);
+
+        List<Pedido> clientes = typedQuery.getResultList();
+
+        Assert.assertFalse(clientes.isEmpty());
+
+        logger.info("Pedido paginados, com limte máximo de 10");
+        clientes.forEach(p -> logger.info("Nome" + " - " + p.getId()));
+    }
+
+}
