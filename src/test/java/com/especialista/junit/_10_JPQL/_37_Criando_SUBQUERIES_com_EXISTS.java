50e3a668ac4dfe56040f1a36abe16b734c055eba
igor-lourenco <igor.lourencosantos@gmail.com>
2026-02-28 14:50:04 -0300

    Criando subqueries com EXISTS e NOT EXISTS

---
+++ src/test/java/com/especialista/junit/_10_JPQL/_37_Criando_SUBQUERIES_com_EXISTS.java
@@ -0,0 +1,66 @@
+package com.especialista.junit._10_JPQL;
+
+import com.especialista.jpa._7_mapeandoEntidadesParaGeracaoDeDDL.modelos.Produto;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class _37_Criando_SUBQUERIES_com_EXISTS extends EntityManagerTest {
+
+/*   - EXISTS é um predicado booleano usado em subconsultas dentro de WHERE (ou HAVING).
+       - Ele não retorna dados; apenas responde: “existe pelo menos uma linha na subquery que satisfaça as condições?”.
+*/
+
+
+    @Test
+    public void usandoExpresao_EXISTS() { // busca todos os produtos que já foram pedidos alguma vez
+
+//      O '1' é uma semântica pra deixar clara a intenção: comunica que só queremos saber se há linhas. Evitando a impressão de que o valor selecionado será usado.
+        String jpql = "SELECT pro"
+            + " FROM Produto pro " // percorre todos os registros de Produto
+            + " WHERE EXISTS ( "
+            + "     SELECT 1 "
+            + "      FROM ItemPedido item"
+            + "      JOIN item.produto pro2 "
+            + "      WHERE pro2 = pro "
+            + ")";
+
+        logger.info("Buscando Produto...");
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        logger.info("Resultado...");
+        lista.forEach(c -> logger.info("ID: " + c.getId()));
+    }
+
+    @Test
+    public void usandoExpresao_NOT_EXISTS() { // busca todos os produtos que nunca foram pedidos alguma vez
+
+//      O '1' é uma semântica pra deixar clara a intenção: comunica que só queremos saber se há linhas. Evitando a impressão de que o valor selecionado será usado.
+        String jpql = "SELECT pro"
+            + " FROM Produto pro "  // percorre todos os registros de Produto
+            + " WHERE NOT EXISTS ( "
+            + "     SELECT 1 "
+            + "      FROM ItemPedido item"
+            + "      JOIN item.produto pro2 "
+            + "      WHERE pro2 = pro "
+            + ")";
+
+        logger.info("Buscando Produto...");
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        logger.info("Resultado...");
+        lista.forEach(c -> logger.info("ID: " + c.getId()));
+    }
+
+}
