2dff8ceff6ff5a2fa8605761d0cda5344e570576
igor-lourenco <igor.lourencosantos@gmail.com>
2026-03-03 10:24:16 -0300

    Operações em lote - ATUALIZAÇÃO

---
+++ src/test/java/com/especialista/junit/_10_JPQL/_43_Atualizacao_em_lote.java
@@ -0,0 +1,30 @@
+package com.especialista.junit._10_JPQL;
+
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Test;
+
+import javax.persistence.Query;
+
+public class _43_Atualizacao_em_lote extends EntityManagerTest {
+
+
+    @Test
+    public void atualizarEmLote() {
+
+
+        String jpql = "UPDATE ProdutoIniciandoComJPA p1 "
+            + " SET p1.preco = 1000 "
+            + " WHERE LENGTH(p1.nome) > 27 "
+            + " AND p1.id BETWEEN 100 AND 110";
+
+
+        entityManager.getTransaction().begin();
+
+        Query query = entityManager.createQuery(jpql);
+        query.executeUpdate();
+
+
+        entityManager.getTransaction().commit();
+
+    }
+}
