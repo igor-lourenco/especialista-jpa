2873839fa94d6cee55a4b753eb8351ac85a2c5f1
igor-lourenco <igor.lourencosantos@gmail.com>
2026-03-05 10:33:31 -0300

    Usando Tuple para projeções

---
+++ src/test/java/com/especialista/junit/_11_Criteria_API/_5_Usando_Tuple_para_projecoes.java
@@ -0,0 +1,74 @@
+package com.especialista.junit._11_Criteria_API;
+
+import com.especialista.jpa._7_mapeandoEntidadesParaGeracaoDeDDL.modelos.Produto;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class _5_Usando_Tuple_para_projecoes extends EntityManagerTest {
+
+/*
+        - Tuple é usada para representar resultados parciais de consultas, especialmente quando
+        não retorna entidades inteiras, mas sim colunas/expressões específicas (projeções).
+        Como se fosse uma “linha” com vários campos nomeados e tipados, mais legível e segura do que usar Object[].
+*/
+    @Test
+    public void projetarOResultado(){
+
+//      Convertendo uma JPQL em Criteria Query
+//      String jpql = "SELECT p.id, p.nome FROM Produto p";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery(); // SELECT p.id, p.nome
+
+        Root<Produto> root = criteriaQuery.from(Produto.class); // FROM Pedido p
+
+//        criteriaQuery.multiselect(root.get("id"), root.get("nome")); // atributos p.id, p.nome
+        criteriaQuery.select(criteriaBuilder.tuple(root.get("id").alias("id"), // O mesmo que acima mas usando o Tuple
+            root.get("nome").alias("nome")
+        ));
+
+
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Tuple> lista = typedQuery.getResultList();
+
+        Assert.assertNotNull(lista);
+
+        logger.info("Resultado: " + lista.size());
+        lista.forEach(a -> logger.info("ID: " + a.get("id") + ", Nome: " + a.get("nome")));
+    }
+
+    @Test
+    public void projetarOResultado2(){
+
+//      Convertendo uma JPQL em Criteria Query
+//      String jpql = "SELECT p.id, p.nome FROM Produto p";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery(); // SELECT p.id, p.nome
+
+        Root<Produto> root = criteriaQuery.from(Produto.class); // FROM Pedido p
+
+//        criteriaQuery.multiselect(root.get("id"), root.get("nome")); // atributos p.id, p.nome
+        criteriaQuery.select(criteriaBuilder.tuple(root.get("id"), root.get("nome"))); // O mesmo que acima mas usando o Tuple
+
+
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Tuple> lista = typedQuery.getResultList();
+
+        Assert.assertNotNull(lista);
+
+        logger.info("Resultado: " + lista.size());
+        lista.forEach(a -> logger.info("ID: " + a.get(0) + ", Nome: " + a.get(1)));
+    }
+
+}
