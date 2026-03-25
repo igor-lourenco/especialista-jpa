673ab09b4ba699425f6ee2cf71289b98e1c66a6f
igor-lourenco <igor.lourencosantos@gmail.com>
2026-03-25 10:58:15 -0300

    Passando parâmtro e executando SQL

---
+++ src/test/java/com/especialista/junit/_12_consultas_nativas/_3_Passando_parametros.java
@@ -0,0 +1,48 @@
+package com.especialista.junit._12_consultas_nativas;
+
+import com.especialista.jpa._7_mapeandoEntidadesParaGeracaoDeDDL.modelos.Produto;
+import com.especialista.junit.utils.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class _3_Passando_parametros extends EntityManagerTest {
+
+    @Test
+    public void usando_SQLResultSetMapping(){
+
+        String sql = "SELECT * "  // Obs: Pra cada Produto é executada mais uma query a mais para buscar o Estoque
+            + " FROM tb_produto "
+            + " WHERE id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+        query.setParameter("id", 1);
+
+        List<Produto> lista = query.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        logger.info("Resultado: " + lista.size());
+        lista.forEach(c -> logger.info(String.format("Produto => ID: %s, Nome: %s", c.getId(), c.getNome())));
+    }
+
+    @Test
+    public void executandoSQL_especificandoAsColunas(){ //  Se não especificar todas as colunas para ser retornadas, solta Exception
+
+        String sql = "SELECT id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto "
+            + " FROM tb_produto "                    // Obs: Pra cada Produto é executada mais uma query a mais para buscar o Estoque
+            + " WHERE id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+        query.setParameter("id", 1);
+
+        List<Produto> lista = query.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        logger.info("Resultado: " + lista.size());
+        lista.forEach(c -> logger.info(String.format("Produto => ID: %s, Nome: %s", c.getId(), c.getNome())));
+    }
+}
