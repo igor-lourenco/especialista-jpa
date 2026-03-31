1705d3e93bb05d53018957dd3018bcbed9095040
igor-lourenco <igor.lourencosantos@gmail.com>
2026-03-31 11:49:45 -0300

    Usando o HikariCP como gerenciador do pool de conexões

---
+++ src/test/java/com/especialista/junit/_13_Bean_Validation_Pool_de_conexoes_Entity_Graph_e_detalhes_avancados/_2_Pool_de_conexoes/_2_Usando_HikariCP_como_gerenciados_do_pool_de_conexoes.java
@@ -0,0 +1,13 @@
+package com.especialista.junit._13_Bean_Validation_Pool_de_conexoes_Entity_Graph_e_detalhes_avancados._2_Pool_de_conexoes;
+
+import com.especialista.junit.utils.EntityManagerTest;
+
+public class _2_Usando_HikariCP_como_gerenciados_do_pool_de_conexoes extends EntityManagerTest {
+
+    /*
+        - Documentação do HikariCP:
+
+            - https://github.com/brettwooldridge/HikariCP
+
+     */
+}
